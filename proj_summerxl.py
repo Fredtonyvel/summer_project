c1684e8780dcca7aa28b028985cd5e1298951b48
fredtonyvel <vfredanthony@yahoo.com>
2016-07-16 16:08:34 -0400

    Added modifications to code using for loops

--- proj_summerxl.py
+++ proj_summerxl.py
@@ -2,10 +2,21 @@ import openpyxl
 import os
 from openpyxl.workbook import Workbook
 from openpyxl.styles import Font, Fill
+from openpyxl.styles import colors
 from datetime import datetime
 
 now = datetime.now()
 #print now.strftime("%m/%d/%Y %H:%M:%S")
+
+f=open('ID_Data.txt', 'r')
+Id=f.read()
+ID = Id.split('\n')
+print ID
+
+g=open('Name.txt', 'r')
+name=g.read()
+Name = name.split('\n')
+print Name
 
 #loading workbook => summer_projxl.xlsx
 wb = openpyxl.load_workbook('summer_projxl.xlsx')
@@ -14,85 +25,76 @@ type(wb)
 ws = wb.active	#Allows you to write into speadsheet
 
 #Changing column dimensions
-ws.column_dimensions['A'].width = 20
-ws.column_dimensions['B'].width = 20
-ws.column_dimensions['C'].width = 15
-ws.column_dimensions['D'].width = 15
+a = ws.column_dimensions['A'].width = 20
+b = ws.column_dimensions['B'].width = 20
+c = ws.column_dimensions['C'].width = 15
+d = ws.column_dimensions['D'].width = 15
 
+#Changing Font colors
+#a.font = Font(color=colors.RED)
+#b.font = Font(color=colors.BLUE)
 
-c = ws['A1']		
-d = ws['B1']
-e = ws['C1']
-f = ws['D1']
+e = ws['A1']		
+f = ws['B1']
+g = ws['C1']
+h = ws['D1']
 
 #Applying style changes to each object
-c.font = Font(bold=True)
-d.font = Font(bold=True)	
-e.font = Font(bold=True)
-f.font = Font(bold=True)
+e.font = Font(bold=True, size=14)
+f.font = Font(bold=True, size=14)	
+g.font = Font(bold=True, size=14)
+h.font = Font(bold=True, size=14)
 
 #Writing values to cells (Title of each column)
 sheet = wb.get_sheet_by_name('Sheet1')	#Picked Sheet1
+
 sheet['A1'] = 'ID Data'
 sheet['B1'] = 'Student\'s Name'
 sheet['C1'] = 'Date'
 sheet['D1'] = 'Time'
 
-#Adding values to each cell under appropiate column
-sheet['A2'] = '515'
-sheet['B2'] = 'Freddy'
-sheet['C2'] = now.strftime("%m/%d/%Y")  #format mm/dd/yyyy
-sheet['D2'] = now.strftime("%H:%M:%S")  #format hh:mm
+id_count = 0
+name_count = 0
 
-sheet['A3'] = '428'
-sheet['B3'] = 'Gary'
-sheet['C3'] = now.strftime("%m/%d/%Y")
-sheet['D3'] = now.strftime("%H:%M:%S")
+for p in ID:
+	id_count += 1
+print "\nNumber of items in ID_Data.txt are: ", id_count
 
-sheet['A4'] = '596'
-sheet['B4'] = 'Ashok'
-sheet['C4'] = now.strftime("%m/%d/%Y")
-sheet['D4'] = now.strftime("%H:%M:%S")
+for i in range (2, id_count):
+	sheet['A' + str(i)] = ID[i]
+	print i, ' = ', ID[i]
 
-sheet['A5'] = '354'
-sheet['B5'] = 'Timothy'
-sheet['C5'] = now.strftime("%m/%d/%Y")
-sheet['D5'] = now.strftime("%H:%M:%S")
+print "\n"
 
-sheet['A6'] = '896'
-sheet['B6'] = 'Paul'
-sheet['C6'] = now.strftime("%m/%d/%Y")
-sheet['D6'] = now.strftime("%H:%M:%S")
+for p in ID:
+	name_count += 1
+print "\nNumber of items in Name.txt are: ", name_count
 
-sheet['A7'] = '596'
-sheet['B7'] = 'Jasmine'
-sheet['C7'] = now.strftime("%m/%d/%Y")
-sheet['D7'] = now.strftime("%H:%M:%S")
+for j in range (2,name_count):
+	sheet['B' + str(j)] = Name[j]
+	print j, ' = ', Name[j]
 
-sheet['A8'] = '381'
-sheet['B8'] = 'Xavier'
-sheet['C8'] = now.strftime("%m/%d/%Y")
-sheet['D8'] = now.strftime("%H:%M:%S")
+print "\n"
 
-sheet['A9'] = '786'
-sheet['B9'] = 'Jane'
-sheet['C9'] = now.strftime("%m/%d/%Y")
-sheet['D9'] = now.strftime("%H:%M:%S")
+for k in range (2,12):
+	date = now.strftime("%m/%d/%Y")
+	sheet['C' + str(k)] = date
+	print k, ' = ', date
 
-sheet['A10'] = '685'
-sheet['B10'] = 'Bob'
-sheet['C10'] = now.strftime("%m/%d/%Y")
-sheet['D10'] = now.strftime("%H:%M:%S")
+print "\n"
 
-sheet['A11'] = '262'
-sheet['B11'] = 'Zack'
-sheet['C11'] = now.strftime("%m/%d/%Y")
-sheet['D11'] = now.strftime("%H:%M:%S")
+for l in range (2,12):
+	time = now.strftime("%H:%M:%S")
+	sheet['D' + str(l)] = time
+	print l, ' = ', time
 
+'''
+#For loop to print out values in each cell
 for rowOfCellObjects in sheet['A1': 'D11']:
 	for cellObj in rowOfCellObjects:
 		print(cellObj.coordinate, cellObj.value)
 	print('--- END OF ROW ---\n')
+'''
 
 wb.save('summer_projxl.xlsx')	#Saves updates on excel file
 
