716bb4dd49946c023c4bbcd42fc61f0ca83af37a
Fredtonyvel <vfredanthony@yahoo.com>
2016-08-14 21:16:32 -0400

    Defined a new function, but still not working properly yet

--- month_pyxl.py
+++ month_pyxl.py
@@ -1,8 +1,8 @@
 import openpyxl
 import calendar
-import os
 from datetime import datetime
 from openpyxl.cell import get_column_letter
+import os
 
 #loading workbook => summer_projxl.xlsx
 wb = openpyxl.load_workbook('month_pyxl.xlsx')
@@ -12,11 +12,39 @@ ws = wb.active	#Allows you to write into speadsheet
 #Writing values to cells (Title of each column)
 sheet = wb.get_sheet_by_name('Sheet1')	#Picked Sheet1
 
-sheet['A1'] = 'Date'
-sheet['B1'] = 'Time'
+def sheet_title():
+	sheet_count = sheet.max_column
+	
+	sheet[get_column_letter(sheet_count) + str(1)] = 'ID_data'
+	sheet_count += 1
+	
+	sheet[get_column_letter(sheet_count) + str(1)] = 'Name'
+	sheet_count += 1
+	
+	sheet[get_column_letter(sheet_count) + str(1)] = 'Date'
+	sheet_count += 1
+	
+	sheet[get_column_letter(sheet_count) + str(1)] = 'Time'
+	sheet_count += 1
+
+	print "Max_column is: ", get_column_letter(sheet_count)
+
+	pass
+
+sheet_title()
+
+
+'''
+sheet['A1'] = 'ID_data'
+sheet['B1'] = 'Name'
+sheet['C1'] = 'Date'
+sheet['D1'] = 'Time'
 
 print sheet['A1'].value
 print sheet['B1'].value
+print sheet['C1'].value
+print sheet['D1'].value
+'''
 
 
 
@@ -25,8 +53,22 @@ print sheet['B1'].value
 now = datetime.now()
 
 for i in range (2, 11):
+	id_data = raw_input("Enter ID #: ")
+	sheet['A' + str(i)] = id_data
+	print i, ' = ', id_data
+
+print "\n"
+
+for i in range (2, 11):
+	name_data = raw_input("Enter student's name: ")
+	sheet['B' + str(i)] = name_data
+	print i, ' = ', name_data
+
+print "\n"
+
+for i in range (2, 11):
 	date = now.strftime("%m/%d/%Y")
-	sheet['A' + str(i)] = date
+	sheet['C' + str(i)] = date
 	print i, ' = ', date
 
 print "\n"
@@ -34,7 +76,7 @@ print "\n"
 for l in range (2,11):
 	#time = now.strftime("%H:%M:%S")
 	time = now.strftime("%H:%M")
-	sheet['B' + str(l)] = time
+	sheet['D' + str(l)] = time
 	print l, ' = ', time
 
 print "\nsheet.max_row =", sheet.max_row
@@ -63,7 +105,18 @@ else:
 	print "Didn't work!"
 '''
 
-if (sheet_value == '13:18'):
+
+if (sheet_value == '21:12'):
+	sheet_count = sheet.max_column
+	sheet_count += 3
+	sheet_title()
+
+else:
+	print "Didn't work!"
+
+
+'''
+if (sheet_value == '16:20'):
 	sheet_count = sheet.max_column
 	sheet_count += 3
 	print "New sheet.max_column =", get_column_letter(sheet_count)
@@ -71,7 +124,7 @@ if (sheet_value == '13:18'):
 	sheet[get_column_letter(sheet_count) + str(1)] = 'New_Date'
 else:
 	print "Didn't work!"
-
+'''
 
 
 #new_sheet_count += 1
